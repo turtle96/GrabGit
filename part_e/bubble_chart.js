86bfec1131997c7019bd3930464f2e4139331949
Vivian Low <vivian198912@gmail.com>
2017-04-13 01:27:00 +0800

    tooltip working now

--- part_e/bubble_chart.js
+++ part_e/bubble_chart.js
@@ -13,7 +13,18 @@
   .append('svg')
   .attr("width", diameter)
   .attr("height", diameter)
-  .attr("class", "bubble");
+  .append("g")
+  .attr("transform", "translate(0,0)");
+
+  var tooltip = d3.select('#chart')
+  .append('div')
+  .attr('class', 'tooltip');
+  
+  tooltip.append('div')
+    .attr('class', 'name');
+
+  tooltip.append('div')
+    .attr('class', 'value');
 
   d3.csv("locStatsCSV.csv", function(error, data){
 
@@ -23,19 +34,30 @@
     //bubbles needs very specific format, convert data to this.
     var nodes = bubble.nodes({children:data}).filter(function(d) { return !d.children; });
 
-    //setup the chart
-    var bubbles = svg.append("g")
-    .attr("transform", "translate(0,0)")
-    .selectAll(".bubble")
-    .data(nodes)
-    .enter();
-
     //create the bubbles
-    bubbles.append("circle")
-      .attr("r", function(d){ return d.r; })
-      .attr("cx", function(d){ return d.x; })
-      .attr("cy", function(d){ return d.y; })
-      .style("fill", function(d) { return color(d.value); });
+    var bubbles = svg.selectAll("circle")
+    .data(nodes)
+    .enter()
+    .append("circle")
+    .attr("r", function(d){ return d.r; })
+    .attr("cx", function(d){ return d.x; })
+    .attr("cy", function(d){ return d.y; })
+    .style("fill", function(d) { return color(d.value); });
+
+    bubbles.on('mouseover', function(d) {
+      tooltip.select('.name').html(d.name);
+      tooltip.select('.value').html(d.value + " lines of code");
+      tooltip.style('display', 'block');
+    });
+
+    bubbles.on('mouseout', function(d) {
+      tooltip.style('display', 'none');
+    });
+
+    bubbles.on('mousemove', function(d) {
+      tooltip.style('top', (d3.event.layerY + 10) + 'px')
+        .style('left',(d3.event.layerX + 10) + 'px');
+    });
 
   });
 
