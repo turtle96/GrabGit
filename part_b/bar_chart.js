28d24146d532dc3669886a8ec24abc9357fc3fc1
turtle96 <angjazlyn@hotmail.com>
2017-04-14 14:21:27 +0800

    refactor and refine form submission also can submit by pressing enter key now

--- part_b/bar_chart.js
+++ part_b/bar_chart.js
@@ -1,284 +1,300 @@
-var data;
+function display(names) {	
+	var data;
 
-var height = 500;
-var width = 1000;
-var margin = 50;
+	var height = 500;
+	var width = 1000;
+	var margin = 50;
 
-var colorPositive = d3.scale.ordinal().range(["#00BFA5", "#80CBC4", "#26A69A", "#00796B", "#004D40"]);
-var colorNegative = d3.scale.ordinal().range(["#FF5252", "#EF9A9A", "#EF5350", "#D32F2F", "#B71C1C"]);
+	var colorPositive = d3.scale.ordinal().range(["#00BFA5", "#80CBC4", "#26A69A", "#00796B", "#004D40"]);
+	var colorNegative = d3.scale.ordinal().range(["#FF5252", "#EF9A9A", "#EF5350", "#D32F2F", "#B71C1C"]);
 
-var svg = d3.select("#chartDisplay")
-		.append("svg")
-		.attr("height", height)
-		.attr("width", width);
+	var svg = d3.select("#chartDisplay")
+			.append("svg")
+			.attr("height", height)
+			.attr("width", width);
 
-var url = "http://kfwong.com:3000/api/repos/tungnk1993/scrapy/1bc";
-var dataJsonFilePath = "data/data.json";
+	var url = "http://kfwong.com:3000/api/repos/tungnk1993/scrapy/1bc";
+	var dataJsonFilePath = "data/data.json";	//for testing
 
-d3.json(url, function(error, data) {
-	var dataSet = {};
-	var currentDate, currentMonth = null, currentName, monthKey;
-	var keys = [];
-	var added = 0, deleted = 0;
+	d3.json(url, function(error, data) {
+		var dataSet = {};
+		var currentDate, currentMonth = null, currentName, monthKey;
+		var keys = [];
+		var added = 0, deleted = 0;
 
-	var minDate = null, maxDate = null;
-	
-	console.log(data);
+		var minDate = null, maxDate = null;
+		
+		console.log(data);
 
-	data.forEach(function(author){
-		//console.log(author);
-		keys.push(author[0].name);
+		data = data.filter(function(author) {
+			var result = false;
 
-		author.forEach(function(d){
-			//console.log(d);
-			currentDate = moment(d.date, "MM/DD/YYYY");
-			currentName = d.name;
+			names.forEach(function(name) {
+				if (name.toLowerCase() == author[0].name.toLowerCase()) {
+					result = true;
+				}
+			}); 
 
-			if (currentMonth != null && currentMonth.isSame(currentDate, "month")) {
-				added += +d.add;
-				deleted += +d.delete * -1;
+			if (result) {
+				return author;
 			}
-			
-			if (currentMonth != null && dataSet[monthKey][currentName]) {
-				dataSet[monthKey][currentName].added = added;
-				dataSet[monthKey][currentName].deleted = deleted;
-				dataSet[monthKey][currentName].date = monthKey;
-			}
-
-			currentMonth = currentDate;
-			currentMonth.date(1);
-			currentMonth.startOf('day');
-			monthKey = moment(currentMonth);
-
-			if (minDate==null || currentMonth.isBefore(minDate)) {
-				minDate = moment(currentMonth);
-			}
-			if (maxDate==null || currentMonth.isAfter(maxDate)) {
-				maxDate = moment(currentMonth);
-			}
-
-			if (!(dataSet[monthKey])) {
-				dataSet[monthKey] = {date: monthKey};
-				dataSet[monthKey][currentName] = {name: currentName, list: []};
-				added = +d.add;
-				deleted = +d.delete * -1;
-			}
-			else if (!(dataSet[monthKey][currentName])) {
-				dataSet[monthKey][currentName] = {name: currentName, list: []};
-				added = +d.add;
-				deleted = +d.delete * -1;
-			}
-
-			dataSet[monthKey][currentName].list.push(d);
-			
 		});
 
-		dataSet[monthKey][currentName].added = added;
-		dataSet[monthKey][currentName].deleted = deleted;
-		dataSet[monthKey][currentName].date = monthKey;
-		added = 0;
-		deleted = 0;
-	});
+		data.forEach(function(author){
+			//console.log(author);
+			keys.push(author[0].name);
 
-	//console.log(dataSet);
-	//console.log(keys);
+			author.forEach(function(d){
+				//console.log(d);
+				currentDate = moment(d.date, "MM/DD/YYYY");
+				currentName = d.name;
 
-	minDate.subtract(1, "months");
-	minDate.startOf('day');   
-	maxDate.add(1, "months");
-	maxDate.startOf('day');
-	//console.log(minDate.toString() + ", " + maxDate.toString());
-
- 	var x0 = d3.time.scale()
- 		.domain([minDate, maxDate])
- 		.rangeRound([margin, width-margin]);
- 
-	var x1 = d3.scale.ordinal();
-
-  	var interval = 0;
-  	var startDate = moment(minDate);
-  	var endDate = moment(maxDate);
-
-  	while (startDate.isBefore(endDate, "month")) {
-  		interval += 1;
-  		startDate.add(1, "months");
-  	}
-
-  	//console.log("Interval: " + interval);
-  	var bandwidth = (width-margin*2)/interval;
-  	x1.domain(keys).rangeRoundBands([0, bandwidth], 0, 0.2*keys.length);	
-
-  	var y = d3.scale.linear()
-		.rangeRound([height-margin, margin]);
-
-	var yMax = d3.max(d3.values(dataSet), function(d) { /*console.log(d);*/ return d3.max(keys, function(key) { 
-		if (d[key])
-			return d[key].added; 
-		return 0;
-	}); });
-
-	var yMin = d3.min(d3.values(dataSet), function(d) { return d3.min(keys, function(key) {
-		if (d[key]) {
-			return d[key].deleted; 
-		}
-		return 0;
-	}); });
-
-	//console.log("y axis range: " + yMin + ", " + yMax);
-
-	y.domain([yMin, yMax]);
-
-	var xAxis = d3.svg.axis()
-    .scale(x0)
-    .orient("bottom")
-    .tickFormat(d3.time.format("%b %Y"));
-
-	var yAxis = d3.svg.axis()
-    .scale(y)
-    .orient("left");
-
-    colorPositive.domain(keys);
-    colorNegative.domain(keys);
-
-    path = svg.selectAll("g")
-    	.data(d3.values(dataSet))
-    	.enter()
-    	.append("g")
-    		.attr("transform", function(d) {return "translate(" + x0(d.date) + ",0)"; })
-    	.selectAll(".rect")
-    	.data(function(d) { 
-    		var values = [];
-    		keys.map(function(key) {  
-	    		if (d[key]) {
-	    			values.push({key: key, value: d[key].added, date: d[key].date, list: d[key].list}); 
-	    			values.push({key: key, value: d[key].deleted, date: d[key].date, list: d[key].list});
-	    		} 
-    		});
-    		
-    		//console.log(values);
-    		return values; 
-    	})
-	    	.enter().append("rect")
-	    	.attr("x", function(d) { return x1(d.key) - bandwidth/2; })
-	        .attr("y", function(d) { 
-	        	if (d.value > 0) {
-	        		return y(d.value);
-	        	}
-	        	else {
-	        		return y(0);
-	        	} 
-	        })
-	        .attr("width", x1.rangeBand())
-	        .attr("height", function(d) { 
-	        	if (d.value > 0) {
-	        		return y(0) - y(d.value);
-	        	}
-	        	else {
-	        		return y(0) - y(d.value * -1);
-	        	}
-	         })
-	        .attr("fill", function(d) { 
-	        	if (d.value > 0) {
-	        		return colorPositive(d.key); 
-	        	}
-	        	else {
-	        		return colorNegative(d.key);
-	        	}
-	        });
+				if (currentMonth != null && currentMonth.isSame(currentDate, "month")) {
+					added += +d.add;
+					deleted += +d.delete * -1;
+				}
 				
-	path.on("mouseover", function(d) { 
-		d3.select(this).style("opacity", 0.6);
+				if (currentMonth != null && dataSet[monthKey][currentName]) {
+					dataSet[monthKey][currentName].added = added;
+					dataSet[monthKey][currentName].deleted = deleted;
+					dataSet[monthKey][currentName].date = monthKey;
+				}
 
-		tooltip.select(".text").html(d.value + " lines<br>" + d.key + "<br>" + d.date.format("MMM YYYY").toString());
-		tooltip.style("display", "block"); 
-	});
+				currentMonth = currentDate;
+				currentMonth.date(1);
+				currentMonth.startOf('day');
+				monthKey = moment(currentMonth);
 
-	path.on("mouseout", function() { 
-		d3.select(this).style("opacity", 1)
-		tooltip.style("display", "none"); 
-	});
-	path.on("mousemove", function(d) {
-	    tooltip.style('top', (d3.event.layerY + 10) + "px")
-    		.style('left', (d3.event.layerX + 10) + "px");
-	});
+				if (minDate==null || currentMonth.isBefore(minDate)) {
+					minDate = moment(currentMonth);
+				}
+				if (maxDate==null || currentMonth.isAfter(maxDate)) {
+					maxDate = moment(currentMonth);
+				}
 
-	//console.log("y(0): " + y(0));
-	
-	svg.append("g")
-		.attr("class", "axis x")
-		.attr("transform", "translate(0 " + y(0) + ")")
-		.call(xAxis);
+				if (!(dataSet[monthKey])) {
+					dataSet[monthKey] = {date: monthKey};
+					dataSet[monthKey][currentName] = {name: currentName, list: []};
+					added = +d.add;
+					deleted = +d.delete * -1;
+				}
+				else if (!(dataSet[monthKey][currentName])) {
+					dataSet[monthKey][currentName] = {name: currentName, list: []};
+					added = +d.add;
+					deleted = +d.delete * -1;
+				}
 
-	svg.append("g")
-		.attr("class", "axis y")
-		.attr("transform", "translate(" + margin + " 0)")
-		.call(yAxis);
+				dataSet[monthKey][currentName].list.push(d);
+				
+			});
 
-	var legend = svg.append("g")
-		.attr("text-anchor", "end")
-	    .selectAll("g")
-	    .data(keys)
-	    .enter().append("g")
-	      .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+			dataSet[monthKey][currentName].added = added;
+			dataSet[monthKey][currentName].deleted = deleted;
+			dataSet[monthKey][currentName].date = monthKey;
+			added = 0;
+			deleted = 0;
+		});
 
-	legend.append("rect")
-		.attr("x", width - 40)
-		.attr("width", 20)
-		.attr("height", 20)
-		.attr("fill", function(d) { return colorPositive(d); })
-		.style("stroke", "#fff")
-		.style("stroke-width", 2);
+		//console.log(dataSet);
+		//console.log(keys);
 
-    legend.append("rect")
-		.attr("x", width - 20)
-		.attr("width", 20)
-		.attr("height", 20)
-		.attr("fill", function(d) { return colorNegative(d); })
-		.style("stroke", "#fff")
-		.style("stroke-width", 2);
+		minDate.subtract(1, "months");
+		minDate.startOf('day');   
+		maxDate.add(1, "months");
+		maxDate.startOf('day');
+		//console.log(minDate.toString() + ", " + maxDate.toString());
 
-	legend.append("text")
-		.attr("x", width - 50)
-		.attr("y", 16)
-		.text(function(d) { return d; });
+	 	var x0 = d3.time.scale()
+	 		.domain([minDate, maxDate])
+	 		.rangeRound([margin, width-margin]);
+	 
+		var x1 = d3.scale.ordinal();
 
-	/******** Show Commit History ********/
-	path.on("click", function(d) {
-	    //console.log(d.list);
-	    $("#helpHeader").hide();
-	    $("#commitContainer").find("div").remove(".panel");
-	    var commitDisplay = d3.select("#commitContainer");
+	  	var interval = 0;
+	  	var startDate = moment(minDate);
+	  	var endDate = moment(maxDate);
 
-	    commitDisplay.selectAll("div")
-	    	.data(d.list)
+	  	while (startDate.isBefore(endDate, "month")) {
+	  		interval += 1;
+	  		startDate.add(1, "months");
+	  	}
+
+	  	//console.log("Interval: " + interval);
+	  	var bandwidth = (width-margin*2)/interval;
+	  	x1.domain(keys).rangeRoundBands([0, bandwidth], 0, 0.2*keys.length);	
+
+	  	var y = d3.scale.linear()
+			.rangeRound([height-margin, margin]);
+
+		var yMax = d3.max(d3.values(dataSet), function(d) { /*console.log(d);*/ return d3.max(keys, function(key) { 
+			if (d[key])
+				return d[key].added; 
+			return 0;
+		}); });
+
+		var yMin = d3.min(d3.values(dataSet), function(d) { return d3.min(keys, function(key) {
+			if (d[key]) {
+				return d[key].deleted; 
+			}
+			return 0;
+		}); });
+
+		//console.log("y axis range: " + yMin + ", " + yMax);
+
+		y.domain([yMin, yMax]);
+
+		var xAxis = d3.svg.axis()
+	    .scale(x0)
+	    .orient("bottom")
+	    .tickFormat(d3.time.format("%b %Y"));
+
+		var yAxis = d3.svg.axis()
+	    .scale(y)
+	    .orient("left");
+
+	    colorPositive.domain(keys);
+	    colorNegative.domain(keys);
+
+	    path = svg.selectAll("g")
+	    	.data(d3.values(dataSet))
 	    	.enter()
-	    	.append("div").classed("panel panel-custom", true)
-	    	.append("div").classed("panel-body", true)
-	    	.append("h4")
-	    	.attr("align", "left")
-	    	.html(function(d) { 
-	    		var commit = ""; 
-	    		var tabSpacing = "&nbsp;&nbsp;&nbsp;&nbsp;";
+	    	.append("g")
+	    		.attr("transform", function(d) {return "translate(" + x0(d.date) + ",0)"; })
+	    	.selectAll(".rect")
+	    	.data(function(d) { 
+	    		var values = [];
+	    		keys.map(function(key) {  
+		    		if (d[key]) {
+		    			values.push({key: key, value: d[key].added, date: d[key].date, list: d[key].list}); 
+		    			values.push({key: key, value: d[key].deleted, date: d[key].date, list: d[key].list});
+		    		} 
+	    		});
+	    		
+	    		//console.log(values);
+	    		return values; 
+	    	})
+		    	.enter().append("rect")
+		    	.attr("x", function(d) { return x1(d.key) - bandwidth/2; })
+		        .attr("y", function(d) { 
+		        	if (d.value > 0) {
+		        		return y(d.value);
+		        	}
+		        	else {
+		        		return y(0);
+		        	} 
+		        })
+		        .attr("width", x1.rangeBand())
+		        .attr("height", function(d) { 
+		        	if (d.value > 0) {
+		        		return y(0) - y(d.value);
+		        	}
+		        	else {
+		        		return y(0) - y(d.value * -1);
+		        	}
+		         })
+		        .attr("fill", function(d) { 
+		        	if (d.value > 0) {
+		        		return colorPositive(d.key); 
+		        	}
+		        	else {
+		        		return colorNegative(d.key);
+		        	}
+		        });
+					
+		path.on("mouseover", function(d) { 
+			d3.select(this).style("opacity", 0.6);
 
-	    		commit += "Author: " + d.name + "<br>";
+			tooltip.select(".text").html(d.value + " lines<br>" + d.key + "<br>" + d.date.format("MMM YYYY").toString());
+			tooltip.style("display", "block"); 
+		});
 
-	    		if (d.email == "none@none") {
-	    			d.email = "None";
-	    		}
-	    		commit += "Author's Email: " + d.email + "<br>";
-	    		commit += "Commit Date: " + d.dateAndTime + "<br>";
-	    		commit += "Commit Message:<br>" + tabSpacing + d.message.replace("\n", "<br>&nbsp;&nbsp;&nbsp;") + "<br>";
-	    		commit += "<font color=#689F38>Added</font> " + d.add + " lines<br>";
-	    		commit += "<font color=#D32F2F>Deleted</font> " + d.delete + " lines<br>";
-	    		return commit; 
-	    	});
+		path.on("mouseout", function() { 
+			d3.select(this).style("opacity", 1)
+			tooltip.style("display", "none"); 
+		});
+		path.on("mousemove", function(d) {
+		    tooltip.style('top', (d3.event.layerY + 10) + "px")
+	    		.style('left', (d3.event.layerX + 10) + "px");
+		});
+
+		//console.log("y(0): " + y(0));
+		
+		svg.append("g")
+			.attr("class", "axis x")
+			.attr("transform", "translate(0 " + y(0) + ")")
+			.call(xAxis);
+
+		svg.append("g")
+			.attr("class", "axis y")
+			.attr("transform", "translate(" + margin + " 0)")
+			.call(yAxis);
+
+		var legend = svg.append("g")
+			.attr("text-anchor", "end")
+		    .selectAll("g")
+		    .data(keys)
+		    .enter().append("g")
+		      .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+
+		legend.append("rect")
+			.attr("x", width - 40)
+			.attr("width", 20)
+			.attr("height", 20)
+			.attr("fill", function(d) { return colorPositive(d); })
+			.style("stroke", "#fff")
+			.style("stroke-width", 2);
+
+	    legend.append("rect")
+			.attr("x", width - 20)
+			.attr("width", 20)
+			.attr("height", 20)
+			.attr("fill", function(d) { return colorNegative(d); })
+			.style("stroke", "#fff")
+			.style("stroke-width", 2);
+
+		legend.append("text")
+			.attr("x", width - 50)
+			.attr("y", 16)
+			.text(function(d) { return d; });
+
+		/******** Show Commit History ********/
+		path.on("click", function(d) {
+		    //console.log(d.list);
+		    $("#helpHeader").hide();
+		    $("#commitContainer").find("div").remove(".panel");
+		    var commitDisplay = d3.select("#commitContainer");
+
+		    commitDisplay.selectAll("div")
+		    	.data(d.list)
+		    	.enter()
+		    	.append("div").classed("panel panel-custom", true)
+		    	.append("div").classed("panel-body", true)
+		    	.append("h4")
+		    	.attr("align", "left")
+		    	.html(function(d) { 
+		    		var commit = ""; 
+		    		var tabSpacing = "&nbsp;&nbsp;&nbsp;&nbsp;";
+
+		    		commit += "Author: " + d.name + "<br>";
+
+		    		if (d.email == "none@none") {
+		    			d.email = "None";
+		    		}
+		    		commit += "Author's Email: " + d.email + "<br>";
+		    		commit += "Commit Date: " + d.dateAndTime + "<br>";
+		    		commit += "Commit Message:<br>" + tabSpacing + d.message.replace("\n", "<br>&nbsp;&nbsp;&nbsp;") + "<br>";
+		    		commit += "<font color=#689F38>Added " + d.add + "</font> lines<br>";
+		    		commit += "<font color=#D32F2F>Deleted " + d.delete + "</font> lines<br>";
+		    		return commit; 
+		    	});
+		});
 	});
-});
-    
-var tooltip = d3.select("#chartDisplay")
-	.append("div")
-	.attr("class", "tooltipStyle"); 
+	    
+	var tooltip = d3.select("#chartDisplay")
+		.append("div")
+		.attr("class", "tooltipStyle"); 
 
-tooltip.append("div")
-	.attr("class", "text");
+	tooltip.append("div")
+		.attr("class", "text");
 
+}
--- part_b/controller.js
+++ part_b/controller.js
@@ -1,335 +1,47 @@
+$(document).ready(function(){
+
+	$('#submitBtn').click(function(){
+		submitForm();
+	});
+
+	$(document).keypress(function(e) {
+	    if(e.which == 13) {
+	    	submitForm();
+	    }
+	});
+});
+
+function submitForm() {
+	var form = $('form').serializeArray();
+	form = form.map(function(a) { return a.value;});
+	console.log(form);
+
+	displayChart(form);
+}
+
 function displayChart(names) {
+	var flag = false;
+	names.forEach(function(name) {
+		if (name) {
+			flag = true;
+		}
+	});
+
+	if (!flag) {
+		alert("Please enter a name.");
+		return;
+	}
+
 	$("#chartDisplay").hide();
 	$("#commitDisplay").hide();
 	$("#chartDisplay").empty();	//clear graph
 	$("#commitContainer").find("div").remove(".panel");	//removes only the panels for commits
 
-	// $.getScript("bar_chart.js", function(){
-	// 	$("#helpHeader").show();
-	// 	$("#commitDisplay").slideDown("slow");
-	// 	$("#chartDisplay").slideDown("slow");
-	// });
-
-	getBarChart(names);
-
+	$.getScript("bar_chart.js", function(){
+		display(names);
 		$("#helpHeader").show();
 		$("#commitDisplay").slideDown("slow");
 		$("#chartDisplay").slideDown("slow");
-	
-}
-
-$(document).ready(function(){
-
-	$('#submitBtn').click(function(){
-		var form = $('form').serializeArray();
-		form = form.map(function(a) { return a.value;});
-		console.log(form);
-		
-		displayChart(form);
 	});
-});
-
-function getBarChart(names) {
-	var data;
-
-var height = 500;
-var width = 1000;
-var margin = 50;
-
-var colorPositive = d3.scale.ordinal().range(["#00BFA5", "#80CBC4", "#26A69A", "#00796B", "#004D40"]);
-var colorNegative = d3.scale.ordinal().range(["#FF5252", "#EF9A9A", "#EF5350", "#D32F2F", "#B71C1C"]);
-
-var svg = d3.select("#chartDisplay")
-		.append("svg")
-		.attr("height", height)
-		.attr("width", width);
-
-var url = "http://kfwong.com:3000/api/repos/tungnk1993/scrapy/1bc";
-var dataJsonFilePath = "data/data.json";
-
-d3.json(url, function(error, data) {
-	var dataSet = {};
-	var currentDate, currentMonth = null, currentName, monthKey;
-	var keys = [];
-	var added = 0, deleted = 0;
-
-	var minDate = null, maxDate = null;
-	
-	console.log(data);
-
-	data = data.filter(function(author) {
-		var result = false;
-
-		names.forEach(function(name) {
-			if (name.toLowerCase() == author[0].name.toLowerCase()) {
-				result = true;
-			}
-		}); 
-
-		if (result) {
-			return author;
-		}
-	});
-
-	data.forEach(function(author){
-		//console.log(author);
-		keys.push(author[0].name);
-
-		author.forEach(function(d){
-			//console.log(d);
-			currentDate = moment(d.date, "MM/DD/YYYY");
-			currentName = d.name;
-
-			if (currentMonth != null && currentMonth.isSame(currentDate, "month")) {
-				added += +d.add;
-				deleted += +d.delete * -1;
-			}
-			
-			if (currentMonth != null && dataSet[monthKey][currentName]) {
-				dataSet[monthKey][currentName].added = added;
-				dataSet[monthKey][currentName].deleted = deleted;
-				dataSet[monthKey][currentName].date = monthKey;
-			}
-
-			currentMonth = currentDate;
-			currentMonth.date(1);
-			currentMonth.startOf('day');
-			monthKey = moment(currentMonth);
-
-			if (minDate==null || currentMonth.isBefore(minDate)) {
-				minDate = moment(currentMonth);
-			}
-			if (maxDate==null || currentMonth.isAfter(maxDate)) {
-				maxDate = moment(currentMonth);
-			}
-
-			if (!(dataSet[monthKey])) {
-				dataSet[monthKey] = {date: monthKey};
-				dataSet[monthKey][currentName] = {name: currentName, list: []};
-				added = +d.add;
-				deleted = +d.delete * -1;
-			}
-			else if (!(dataSet[monthKey][currentName])) {
-				dataSet[monthKey][currentName] = {name: currentName, list: []};
-				added = +d.add;
-				deleted = +d.delete * -1;
-			}
-
-			dataSet[monthKey][currentName].list.push(d);
-			
-		});
-
-		dataSet[monthKey][currentName].added = added;
-		dataSet[monthKey][currentName].deleted = deleted;
-		dataSet[monthKey][currentName].date = monthKey;
-		added = 0;
-		deleted = 0;
-	});
-
-	//console.log(dataSet);
-	//console.log(keys);
-
-	minDate.subtract(1, "months");
-	minDate.startOf('day');   
-	maxDate.add(1, "months");
-	maxDate.startOf('day');
-	//console.log(minDate.toString() + ", " + maxDate.toString());
-
- 	var x0 = d3.time.scale()
- 		.domain([minDate, maxDate])
- 		.rangeRound([margin, width-margin]);
- 
-	var x1 = d3.scale.ordinal();
-
-  	var interval = 0;
-  	var startDate = moment(minDate);
-  	var endDate = moment(maxDate);
-
-  	while (startDate.isBefore(endDate, "month")) {
-  		interval += 1;
-  		startDate.add(1, "months");
-  	}
-
-  	//console.log("Interval: " + interval);
-  	var bandwidth = (width-margin*2)/interval;
-  	x1.domain(keys).rangeRoundBands([0, bandwidth], 0, 0.2*keys.length);	
-
-  	var y = d3.scale.linear()
-		.rangeRound([height-margin, margin]);
-
-	var yMax = d3.max(d3.values(dataSet), function(d) { /*console.log(d);*/ return d3.max(keys, function(key) { 
-		if (d[key])
-			return d[key].added; 
-		return 0;
-	}); });
-
-	var yMin = d3.min(d3.values(dataSet), function(d) { return d3.min(keys, function(key) {
-		if (d[key]) {
-			return d[key].deleted; 
-		}
-		return 0;
-	}); });
-
-	//console.log("y axis range: " + yMin + ", " + yMax);
-
-	y.domain([yMin, yMax]);
-
-	var xAxis = d3.svg.axis()
-    .scale(x0)
-    .orient("bottom")
-    .tickFormat(d3.time.format("%b %Y"));
-
-	var yAxis = d3.svg.axis()
-    .scale(y)
-    .orient("left");
-
-    colorPositive.domain(keys);
-    colorNegative.domain(keys);
-
-    path = svg.selectAll("g")
-    	.data(d3.values(dataSet))
-    	.enter()
-    	.append("g")
-    		.attr("transform", function(d) {return "translate(" + x0(d.date) + ",0)"; })
-    	.selectAll(".rect")
-    	.data(function(d) { 
-    		var values = [];
-    		keys.map(function(key) {  
-	    		if (d[key]) {
-	    			values.push({key: key, value: d[key].added, date: d[key].date, list: d[key].list}); 
-	    			values.push({key: key, value: d[key].deleted, date: d[key].date, list: d[key].list});
-	    		} 
-    		});
-    		
-    		//console.log(values);
-    		return values; 
-    	})
-	    	.enter().append("rect")
-	    	.attr("x", function(d) { return x1(d.key) - bandwidth/2; })
-	        .attr("y", function(d) { 
-	        	if (d.value > 0) {
-	        		return y(d.value);
-	        	}
-	        	else {
-	        		return y(0);
-	        	} 
-	        })
-	        .attr("width", x1.rangeBand())
-	        .attr("height", function(d) { 
-	        	if (d.value > 0) {
-	        		return y(0) - y(d.value);
-	        	}
-	        	else {
-	        		return y(0) - y(d.value * -1);
-	        	}
-	         })
-	        .attr("fill", function(d) { 
-	        	if (d.value > 0) {
-	        		return colorPositive(d.key); 
-	        	}
-	        	else {
-	        		return colorNegative(d.key);
-	        	}
-	        });
-				
-	path.on("mouseover", function(d) { 
-		d3.select(this).style("opacity", 0.6);
-
-		tooltip.select(".text").html(d.value + " lines<br>" + d.key + "<br>" + d.date.format("MMM YYYY").toString());
-		tooltip.style("display", "block"); 
-	});
-
-	path.on("mouseout", function() { 
-		d3.select(this).style("opacity", 1)
-		tooltip.style("display", "none"); 
-	});
-	path.on("mousemove", function(d) {
-	    tooltip.style('top', (d3.event.layerY + 10) + "px")
-    		.style('left', (d3.event.layerX + 10) + "px");
-	});
-
-	//console.log("y(0): " + y(0));
-	
-	svg.append("g")
-		.attr("class", "axis x")
-		.attr("transform", "translate(0 " + y(0) + ")")
-		.call(xAxis);
-
-	svg.append("g")
-		.attr("class", "axis y")
-		.attr("transform", "translate(" + margin + " 0)")
-		.call(yAxis);
-
-	var legend = svg.append("g")
-		.attr("text-anchor", "end")
-	    .selectAll("g")
-	    .data(keys)
-	    .enter().append("g")
-	      .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
-
-	legend.append("rect")
-		.attr("x", width - 40)
-		.attr("width", 20)
-		.attr("height", 20)
-		.attr("fill", function(d) { return colorPositive(d); })
-		.style("stroke", "#fff")
-		.style("stroke-width", 2);
-
-    legend.append("rect")
-		.attr("x", width - 20)
-		.attr("width", 20)
-		.attr("height", 20)
-		.attr("fill", function(d) { return colorNegative(d); })
-		.style("stroke", "#fff")
-		.style("stroke-width", 2);
-
-	legend.append("text")
-		.attr("x", width - 50)
-		.attr("y", 16)
-		.text(function(d) { return d; });
-
-	/******** Show Commit History ********/
-	path.on("click", function(d) {
-	    //console.log(d.list);
-	    $("#helpHeader").hide();
-	    $("#commitContainer").find("div").remove(".panel");
-	    var commitDisplay = d3.select("#commitContainer");
-
-	    commitDisplay.selectAll("div")
-	    	.data(d.list)
-	    	.enter()
-	    	.append("div").classed("panel panel-custom", true)
-	    	.append("div").classed("panel-body", true)
-	    	.append("h4")
-	    	.attr("align", "left")
-	    	.html(function(d) { 
-	    		var commit = ""; 
-	    		var tabSpacing = "&nbsp;&nbsp;&nbsp;&nbsp;";
-
-	    		commit += "Author: " + d.name + "<br>";
-
-	    		if (d.email == "none@none") {
-	    			d.email = "None";
-	    		}
-	    		commit += "Author's Email: " + d.email + "<br>";
-	    		commit += "Commit Date: " + d.dateAndTime + "<br>";
-	    		commit += "Commit Message:<br>" + tabSpacing + d.message.replace("\n", "<br>&nbsp;&nbsp;&nbsp;") + "<br>";
-	    		commit += "<font color=#689F38>Added</font> " + d.add + " lines<br>";
-	    		commit += "<font color=#D32F2F>Deleted</font> " + d.delete + " lines<br>";
-	    		return commit; 
-	    	});
-	});
-});
-    
-var tooltip = d3.select("#chartDisplay")
-	.append("div")
-	.attr("class", "tooltipStyle"); 
-
-tooltip.append("div")
-	.attr("class", "text");
-
 
 }
-
-
-
